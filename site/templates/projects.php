9184cbe6479c59348184ad67d88d114bb1e71050
amykw105 <amykw105@gmail.com>
2017-12-18 20:01:13 +0000

    services pages

--- site/templates/projects.php
+++ site/templates/projects.php
@@ -10,6 +10,34 @@
         </div>
       </div>
     </div>
+    
+    <section class="text-section">
+      
+      <div class="wrapper">
+        <br/>
+          <h2 class="page-title">Your brand is unique so your website should be too. </h2>
+          <p class="greeting-intro animated fadeInUp">We don't believe in putting your logo onto a template and calling it a day. We take pride in our custom designs and hand-written code. </p>
+          <div class="greeting-wrapper">
+            <div class="greeting-icon gi-pc">
+              <img src="assets/images/web-design-planning.png" alt="Plymouth Web Design - Planning stage"/>
+              <p class="greeting-header">Planning & Consultancy</p>
+            </div>
+            <div class="greeting-icon gi-d">
+              <img src="assets/images/web-design-template.png" alt="Plymouth Web Design - Design stage"/>
+              <p class="greeting-header">Design</p>
+            </div>
+            <div class="greeting-icon gi-dd">
+              <img src="assets/images/web-design-development.png" alt="Plymouth Web Design - Development stage"/>
+              <p class="greeting-header">Development</p>
+            </div>
+            <div class="greeting-icon gi-m">
+              <img src="assets/images/web-design-systems.png" alt="Plymouth Web Design - Maintenance stage"/>
+              <p class="greeting-header">Care & Maintenance</p>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+      </div>
+    </section> 
       
     <section class="content-section services-section">
       <div class="wrapper"> 
